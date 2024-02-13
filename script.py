d5f3d25cc0241a5c5f545bc908e87a6d03c6cdc2
NickHMetzler <nickfromportugal@gmail.com>
2024-02-13 13:46:03 -0500

    Fixed many issues

--- script.py
+++ script.py
@@ -50,136 +50,107 @@ unique_items = set()
 # Create a list to store the data
 rows = []
 
-# Function to get SKU based on option values
-def get_sku(product, option_values):
-    for variant in product["variants"]:
-        match_count = 0
-        for i, value in enumerate(option_values):
-            if value and variant.get(f"option{i+1}") == value:
-                match_count += 1
-        if match_count == len(option_values):
-            return variant, variant["sku"]
-    return '', ''
+def convert_size(input_string):
+    # Define the mapping of sizes
+    size_mapping = {
+        'xs': 'XS',
+        'sm': 'S',
+        'lg': 'L',
+        'xl': 'XL',
+        'xxl': 'XXL'
+    }
+
+    # Replace the sizes in the input string (case-insensitive)
+    for size, replacement in size_mapping.items():
+        input_string = re.sub(f'(?i){re.escape(size)}', replacement, input_string)
+
+    print("replaced")
+    print(input_string)
+
+    return input_string
 
 
 while True:
     products_data = get_products_json(api_url + str(page_number))
     if products_data and products_data.get("products"):
         save_to_file(products_data, f'products_page_{page_number}.json')
-        print(f"JSON data for page {page_number} saved to 'products_page_{page_number}.json'")
+        #print(f"JSON data for page {page_number} saved to 'products_page_{page_number}.json'")
 
         # Process data for each product on the current page
         for product in products_data['products']:
             item_name = product['title']
             i = 0
+            k = 0
             break_flag = False
             # Check if the item name is already processed
-            if item_name not in unique_items:
+            if item_name not in unique_items and "Gift Card" not in item_name.title() and not "swag high" in item_name.lower():
                 unique_items.add(item_name)
-                
-                # Extract option values based on your structure
-                option_values_to_match = [
-                    product["options"][0]['values'][i] if len(product["options"]) > 0 and product["options"][0]['values'] and len(product["options"][0]['values']) > i else '',
-                    product["options"][1]['values'][i] if len(product["options"]) > 1 and product["options"][1] and product["options"][1]['values'] and len(product["options"][1]['values']) > 0 else '',
-                    product["options"][2]['values'][i] if len(product["options"]) > 2 and product["options"][2] and product["options"][2]['values'] and len(product["options"][2]['values']) > 0 else ''
-                ]
-                
-                if option_values_to_match[0] == '' and option_values_to_match[1] == '' and option_values_to_match[2] == '':
-                    break_flag = True
-                
-                if not break_flag:
-                    # Get Variant and SKU based on option values
-                    variant, sku = get_sku(product, option_values_to_match)
-                    print(variant)
-                    print("Variant above")
-                    row_data = {
-                        'Token': '',
-                        'Item Name': item_name,
-                        'Variation Name': variant['title'] if variant and variant['title'] else '',
-                        'SKU': sku,
-                        'Description': product['body_html'],
-                        'SEO Title': f"{item_name} | {store_name}",
-                        'SEO Description': product['body_html'] if product['body_html'] else '',
-                        'Square Online Item Visibility': 'visible' if variant and variant['available'] else '',
-                        'Shipping Enabled': 'Y' if variant and variant['requires_shipping'] else 'N',
-                        'Delivery Enabled': local_delivery_enabled,
-                        'Pickup Enabled': local_pickup_enabled,
-                        'Price': product['variants'][i]['price'] if product['variants'][i]['price'] else product['variants'][0]['price'] if product['variants'][0]['price'] else '0.00',
-                        'Option Name 1': product["options"][0]['name'] if len(product["options"]) > 0 and product["options"][0] and product["options"][0]['name'] else '',
-                        'Option Value 1': product["options"][0]['values'][i] if len(product["options"]) > 0 and product["options"][0]['values'] and product["options"][0]['values'][i] else '',
-                        'Option Name 2': product["options"][1]['name'] if len(product["options"]) > 1 and product["options"][1] and product["options"][1]['name'] else '',
-                        'Option Value 2': product["options"][1]['values'][i] if len(product["options"]) > 1 and product["options"][1]['values'] and product["options"][1]['values'][i] else '',
-                        'Option Name 3': product["options"][2]['name'] if len(product["options"]) > 2 and product["options"][2] and product["options"][2]['name'] else '',
-                        'Option Value 3': product["options"][2]['values'][i] if len(product["options"]) > 2 and product["options"][2]['values'] and product["options"][2]['values'][i] else '',
-                        f'Current Quantity {new_store_name}': current_quantity,
-                        # Fill other fields with default values
-                        'Category': product['product_type'],
-                        'Permalink': product['handle'] if product['handle'] else '',
-                        'Weight (kg)': (int(variant[0]['grams']/100)) if variant and variant['grams'] else '',
-                        'Self-Serve Ordering Enabled': '',
-                        'Online Sale Price': '',
-                        'Sellable': 'Y',
-                        'Stockable': 'Y',
-                        'Skip Detail Screen in POS': 'N',
-                        f'New Quantity {new_store_name}': '',
-                        f'Stock Alert Enabled {new_store_name}': '',
-                        f'Stock Alert Count {new_store_name}': ''
-                    }
-                    rows.append(row_data)
-
-                    while not break_flag:
+                while k < len(product["variants"]):
+                    option_names = []
+                    option_values = []
+                    
+                    if k < len(product["variants"]):
+                        variant = product["variants"][k]
+                        # Get Variant and SKU based on option values
+                        sku = variant["sku"]
+                        j = 1
+                        #print("Variant")
+                        #print(variant)
+                        while j < 3:
+                            variant_option = variant[f"option{j}"]
+                            #print("\nVariant Option")
+                            #print(variant_option)
+                            if product["options"] and variant_option:
+                                for option in product["options"]:
+                                    if variant_option in option["values"]:
+                                        if option["name"] != "Title":
+                                            #print("\nChosen Name, and Option Match")
+                                            #print(option["name"])
+                                            #print(variant_option)
+                                            option_names.append(option["name"])
+                                            option_values.append(variant_option)
+                            j += 1
+                        #print("\nOption Names and Values")
+                        #print(option_names)
+                        #print(option_values)
+                        k += 1
+                        row_data = {
+                            'Token': '',
+                            'Item Name': item_name.title(),
+                            'Variation Name': convert_size(variant['title'].title()) if variant and variant['title'] and variant['title'].title() != "Default Title" else "",
+                            'SKU': sku,
+                            'Description': product['body_html'],
+                            'SEO Title': f"{item_name.title()} | {store_name}",
+                            'SEO Description': product['body_html'] if product['body_html'] else '',
+                            'Square Online Item Visibility': 'visible' if variant and variant['available'] else 'visible',
+                            'Shipping Enabled': 'Y' if variant and variant['requires_shipping'] else 'N',
+                            'Delivery Enabled': local_delivery_enabled,
+                            'Pickup Enabled': local_pickup_enabled,
+                            'Price': product['variants'][i]['price'] if product['variants'][i]['price'] else product['variants'][0]['price'] if product['variants'][0]['price'] else '0.00',
+                            'Option Name 1': '',#option_names[0] if len(option_names) > 0 else '',
+                            'Option Value 1': '',#option_values[0][:255] if len(option_values) > 0 else '',
+                            'Option Name 2': '',#option_names[1] if len(option_names) > 1 else '',
+                            'Option Value 2': '',#option_values[1][:255] if len(option_values) > 1 else '',
+                            'Option Name 3': '',#option_names[2] if len(option_names) > 2 else '',
+                            'Option Value 3': '',#option_values[2][:255] if len(option_values) > 2 else '',
+                            f'Current Quantity {new_store_name}': current_quantity,
+                            # Fill other fields with default values
+                            'Category': product['product_type'],
+                            'Permalink': product['handle'] if product['handle'] else '',
+                            'Weight (kg)': (int(variant[0]['grams']/100)) if variant and variant['grams'] else '',
+                            'Self-Serve Ordering Enabled': '',
+                            'Online Sale Price': '',
+                            'Sellable': 'Y',
+                            'Stockable': 'Y',
+                            'Skip Detail Screen in POS': 'N',
+                            f'New Quantity {new_store_name}': '',
+                            f'Stock Alert Enabled {new_store_name}': '',
+                            f'Stock Alert Count {new_store_name}': ''
+                        }
+                        rows.append(row_data)
                         i += 1
-                        
-                        # Extract option values based on your structure
-                        option_values_to_match = [
-                            product["options"][0]['values'][i] if len(product["options"]) > 0 and product["options"][0]['values'] and len(product["options"][0]['values']) > i else '',
-                            product["options"][1]['values'][i] if len(product["options"]) > 1 and product["options"][1] and product["options"][1]['values'] and len(product["options"][1]['values']) > i else '',
-                            product["options"][2]['values'][i] if len(product["options"]) > 2 and product["options"][2] and product["options"][2]['values'] and len(product["options"][2]['values']) > i else ''
-                        ]
-                        
-                        if option_values_to_match[0] == '' and option_values_to_match[1] == '' and option_values_to_match[2] == '':
-                            break_flag = True
 
-                        if not break_flag:
-                            # Get Variant and SKU based on option values
-                            variant, sku = get_sku(product, option_values_to_match)
-                            print(variant)
-                            print("Variant above")
-                            row_data = {
-                                'Token': '',
-                                'Item Name': '',
-                                'Variation Name': variant['title'] if variant and variant['title'] else '',
-                                'SKU': sku,
-                                'Description': '',
-                                'SEO Title': '',
-                                'SEO Description': '',
-                                'Square Online Item Visibility': 'visible' if variant and variant['available'] else '',
-                                'Shipping Enabled': '',
-                                'Delivery Enabled': '',
-                                'Pickup Enabled': '',
-                                'Price': product['variants'][i]['price'] if product['variants'][i]['price'] else product['variants'][0]['price'] if product['variants'][0]['price'] else '0.00',
-                                'Option Name 1': '',
-                                'Option Value 1': product["options"][0]['values'][i] if len(product["options"]) > 0 and product["options"][0]['values'] and len(product["options"][0]['values']) > i + 1 and product["options"][0]['values'][i] else '',
-                                'Option Name 2': '',
-                                'Option Value 2': product["options"][1]['values'][i] if len(product["options"]) > 1 and product["options"][1]['values'] and len(product["options"][1]['values']) > i + 1 and product["options"][1]['values'][i] else '',
-                                'Option Name 3': '',
-                                'Option Value 3': product["options"][2]['values'][i] if len(product["options"]) > 2 and product["options"][2]['values'] and len(product["options"][2]['values']) > i + 1 and product["options"][2]['values'][i] else '',
-                                f'Current Quantity {new_store_name}': current_quantity,
-                                # Fill other fields with default values
-                                'Category': '',
-                                'Permalink': '',
-                                'Self-Serve Ordering Enabled': '',
-                                'Online Sale Price': '',
-                                'Sellable': 'Y',
-                                'Stockable': 'Y',
-                                'Skip Detail Screen in POS': 'N',
-                                f'New Quantity {new_store_name}': '',
-                                f'Stock Alert Enabled {new_store_name}': '',
-                                f'Stock Alert Count {new_store_name}': ''
-                            }
-                            if break_flag == True:
-                                break
-                            rows.append(row_data)
+                    
 
         page_number += 1
     else:
@@ -217,7 +188,7 @@ for i in range(1, page_number):
 
                 for i, image in enumerate(product['images']):
                     image_url = image['src']
-                    image_name = clean_filename(f"{item_name.replace(' ', '_')}_{i+1}.jpg")  # Use underscores instead of spaces
+                    image_name = clean_filename(f"{item_name.replace(' ', '_')}_{i+1}.png")  # Use underscores instead of spaces
                     image_path = os.path.join(item_folder, image_name)
 
                     print("Working...")
